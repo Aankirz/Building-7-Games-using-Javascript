60c65b3ce53f85d1cd5afc738eeee60ee0f66a8d
Aankirz <sahuankit453@gmail.com>
2022-07-08 11:48:50 +0530

    Building Game1

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # Building 7 Games using Javascript
  Learning JS by building 7 simple games
+
+Game  from easy to intermediate( Game 1 to 7)
+//Brushing Javascript
+Learning from FCC.
---
+++ Rock-Paper-Scissor-Game_1/app.js
@@ -0,0 +1,6 @@
+const computerChoice=document.getElementById('computer-choice');
+const userChoice=document.getElementById('user-choice');
+const result=document.getElementById('result');
+ 
+//Now we want to get all possible results 
+const possibleChoices=document.querySelectorAll('button');
